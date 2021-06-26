70f2545fd13f7686398e0ddac3d37f35d63b6820
Павел <surkovv.pavel@gmail.com>
2021-06-26 15:48:40 +0700

    Adds JSON file with data

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/normalize.css';
 import './css/style.css';
+
+
+(async function getData() {
+  const response = await fetch('./json/data.json');
+  if(response.ok) {
+    const json = await response.json();
+    console.log(json);
+  } else {
+    alert("Error: " + response.status);
+  }
+})();
+
+
