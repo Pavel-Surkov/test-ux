f72dad6464cd36569bb2d573a1208e6046b1e73d
Павел <surkovv.pavel@gmail.com>
2021-06-27 23:11:31 +0700

    Adds interactive payment block, but renders undefined

--- src/jsx/first-step.jsx
+++ src/jsx/first-step.jsx
@@ -8,5 +8,5 @@ export default function FirstStep(props) {
         <Select courses={props.courses} />
       </div>
     </section>
-);
+  );
 }
--- src/jsx/payment-instruction.jsx
+++ src/jsx/payment-instruction.jsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import FirstStep from "./first-step";
+import SecondStep from "./second-step";
+import ThirdStep from "./third-step";
 
 export default function PaymentInstruction() {
   const [data, setData] = useState({
     'courseType': [],
     'paymentMethods': []
   });
+
+  const finalHeaderRef = useRef(null);
 
   useEffect( async () => {
     const response = await fetch('./json/data.json');
@@ -18,11 +22,29 @@ export default function PaymentInstruction() {
     }
   }, [])
 
+  const handleClickCheckbox = e => {
+    const checkboxes = document.querySelectorAll('.main__payment__block');
+    const finalHeader = finalHeaderRef.current;
+
+    const target = e.target.closest('article');
+    if(target == null) return false;
+    if(target.hasAttribute('checked')) {
+      e.preventDefault();
+    } else {
+      checkboxes.forEach(el => {
+        if(el.hasAttribute('checked')) el.removeAttribute('checked');
+      });
+      target.setAttribute('checked', '');
+      const value = target.dataset.value;
+      finalHeader.innerHTML = `3. Как оплатить через ${value}`;
+    }
+  } 
+
   return (
     <>
       <FirstStep courses={data['courseType']} />
-      {/* <SecondStep payMethods={data['paymentMethods']} />
-      <ThirdStep /> */}
+      <SecondStep payMethods={data['paymentMethods']} onClick={ e => handleClickCheckbox(e) } />
+      <ThirdStep finalHeaderRef={ finalHeaderRef } />
     </>
   );
 }
