49204a7303c39f22699ba248f1577f91abdfb4dc
Павел <surkovv.pavel@gmail.com>
2021-06-28 11:22:22 +0700

    Solves the problem with line breaking in the payment block

--- src/jsx/lower order/select-payment-field.jsx
+++ src/jsx/lower order/select-payment-field.jsx
@@ -7,13 +7,19 @@ export default function SelectPaymentField(props) {
       { methods.map((method, index) => {
         const value = values[index] ? values[index] : method;
 
+        if(method === 'Кредит от Альфа-банка')  {
+          method = 'Кредит <br/>от Альфа-банка';
+        } else if(method === 'Рассрочка от 2 до 9 месяцев по карте «Халва»') {
+          method = 'Рассрочка <br/>от 2 до 9 месяцев <br/>по карте «Халва»';
+        }
+
         return (
           <article 
             className="main__payment__block col-lg-4" 
             data-value={value} 
             onClick={ props.onClick }
             key={value}>
-            <p className="main__payment__method">{method}</p>
+            <p className="main__payment__method" dangerouslySetInnerHTML={{__html: method}}></p>
             <div className="main__payment__radio"></div>
           </article> 
         );
