79b98e717ada416c70a11f0a18991d5df69f1741
Павел <surkovv.pavel@gmail.com>
2021-06-29 21:36:46 +0700

    Changes html classes

--- src/jsx/payment.jsx
+++ src/jsx/payment.jsx
@@ -3,10 +3,10 @@ import PaymentInstructionBlock from "./payment-instruction";
 
 export default function Payment() {
   return (
-    <>
+    <div className="main__wrapper">
       <Breadcrumbs />
       <h1 className="main__header">Оплатить курс</h1>
       <PaymentInstructionBlock />
-    </>
+    </div>
   );
 } 
