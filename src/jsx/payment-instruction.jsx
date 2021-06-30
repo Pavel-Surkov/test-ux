de3057076da3e4be429dd282def87c503d43fedc
Павел <surkovv.pavel@gmail.com>
2021-06-30 20:49:57 +0700

    Nothing changed

--- src/jsx/payment-instruction.jsx
+++ src/jsx/payment-instruction.jsx
@@ -11,7 +11,7 @@ export default function PaymentInstruction() {
 
   const finalHeaderRef = useRef(null);
 
-  useEffect(() => {
+  useEffect( () => {
     (async () => {
       const response = await fetch('./json/data.json');
 
