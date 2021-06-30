3205075a1140bef60ffdd14520eb9e470bef6fd6
Павел <surkovv.pavel@gmail.com>
2021-07-01 00:50:30 +0700

    Solves some conflicts

--- src/index.js
+++ src/index.js
@@ -1,34 +1,7 @@
 import './css/normalize.css';
 import './css/style.css';
+import React from 'react';
+import ReactDom from 'react-dom';
+import Payment from './jsx/payment';
 
-
-(async function getData() {
-  const response = await fetch('./json/data.json');
-  if(response.ok) {
-    const json = await response.json();
-    console.log(json);
-  } else {
-    alert("Error: " + response.status);
-  }
-})();
-
-const checkboxes = document.querySelectorAll('.methods__block');
-const checkboxesBlock = document.querySelector('.methods');
-const finalHeader = document.querySelector('.final__header');
-
-checkboxesBlock.addEventListener('click', e => {
-  const target = e.target.closest('label');
-  if(target == null) return false;
-  if(target.hasAttribute('checked')) {
-    e.preventDefault();
-  } else {
-    checkboxes.forEach(el => {
-      if(el.hasAttribute('checked')) el.removeAttribute('checked');
-    });
-    target.setAttribute('checked', '');
-    const value = target.dataset.value;
-    finalHeader.innerHTML = `3. Как оплатить ${value}`;
-  }
-});
-
-//ReactDOM.render(<Payment />, document.getElementById('main'));
+ReactDOM.render(<Payment />, document.getElementById('main'));
