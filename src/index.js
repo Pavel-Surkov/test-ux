33b3927f26f109248e34a19814dbed870aaac7d8
Павел <surkovv.pavel@gmail.com>
2021-07-01 00:54:18 +0700

    Changes some code to thirdBlock's code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './css/normalize.css';
 import './css/style.css';
-import React from 'react';
-import ReactDom from 'react-dom';
 import Payment from './jsx/payment';
 
 ReactDOM.render(<Payment />, document.getElementById('main'));
