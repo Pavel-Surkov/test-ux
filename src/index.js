e37e50bc5ead81171b5e68bf93720e5d75f3a9d8
Павел <surkovv.pavel@gmail.com>
2021-06-30 15:39:40 +0700

    Changes scss structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './css/normalize.css';
 import './css/style.css';
-import Payment from './jsx/payment';
 
 
 (async function getData() {
